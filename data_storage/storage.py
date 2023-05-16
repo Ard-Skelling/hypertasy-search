2fb6a4cd76ae3991df5a69968ab2ad5ecfc3e86d
anzaikk <599957546@qq.com>
2023-05-16 13:50:58 +0800

    fix bug

--- data_storage/storage.py
+++ data_storage/storage.py
@@ -2,7 +2,7 @@
 Author: anzaikk 599957546@qq.com
 Date: 2023-05-06 20:54:22
 LastEditors: anzaikk 599957546@qq.com
-LastEditTime: 2023-05-16 03:38:53
+LastEditTime: 2023-05-16 13:47:01
 FilePath: /hypertasy-search/data_storage/storage.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -21,8 +21,8 @@ import redis
 
 # 开发环境密码
 REDIS_PASSWORD = 123123
-REDIS_HOST = "172.17.34.175"
-REDIS_PORT = 6380
+REDIS_HOST = "redis"
+REDIS_PORT = 6379
 
 
 # class ClientSingleton(type):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: anzaikk 599957546@qq.com
 Date: 2023-05-09 14:29:24
 LastEditors: anzaikk 599957546@qq.com
-LastEditTime: 2023-05-10 15:20:52
+LastEditTime: 2023-05-16 13:46:33
 FilePath: /hypertasy-search/main.py
 Description: 程序入口
 '''
@@ -10,4 +10,4 @@ from module import app
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8003, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8003, reload=True)
